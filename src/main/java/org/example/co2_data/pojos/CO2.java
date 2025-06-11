0752c5da3bfdb599b0c939e957011d3d183c350f
kueflb21 <kueflb21@htl-kaindorf.at>
2025-06-11 14:38:26 +0200

    Working DB Connection + sensorNr

--- src/main/java/org/example/co2_data/pojos/CO2.java
+++ src/main/java/org/example/co2_data/pojos/CO2.java
@@ -1,25 +1,23 @@
 package org.example.co2_data.pojos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@Data
 public class CO2 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int sensorNr;
-    private float co2_value;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private int sensorNr;
 
-
-    private LocalDateTime dateTime;
+  private float co2;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime date;
 }
