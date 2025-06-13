8e65522a2fe49d41da48051ef7032e665b6fd368
kueflb21 <kueflb21@htl-kaindorf.at>
2025-06-13 11:18:58 +0200

    from-to route

--- src/main/java/org/example/co2_data/web/CO2Controller.java
+++ src/main/java/org/example/co2_data/web/CO2Controller.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -59,6 +57,11 @@ public class CO2Controller {
         Float latestCO2 = co2Repository.getLatestMeasurement(classRoom);
 
         return ResponseEntity.ok(latestCO2);
+    }
+
+    @PostMapping("/api/CO2/post")
+    public CO2 createCO2Entry(@RequestBody CO2 co2) {
+        return co2Repository.save(co2);
     }
 
 }
